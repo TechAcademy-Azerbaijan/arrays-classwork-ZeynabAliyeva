043d5b258c915816aa0e19937bc91766963bc689
ZeynabAliyeva <103027531+ZeynabAliyeva@users.noreply.github.com>
2022-08-29 17:26:24 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -4,7 +4,7 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-   let n = result.input.split(" ");
+    let n = result.input.split(" ");
     let max = n[0];
     let min = n[0];
     for(let i = 0; i<n.length; i++){
@@ -19,6 +19,6 @@ prompt.get('input', function (err, result) {
           
       }
   }
-  let cvb = sum+max
-    console.log(parseInt(cvb));
+  let cvb = parseInt(sum+max)
+    console.log(cvb);
 });
